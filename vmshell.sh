8e76444f71e77d353e52179d972beafcc3e60eb2
mkiruba <mkiruba@gmail.com>
2022-03-27 22:19:08 +0100

    init script

---
+++ vmshell.sh
@@ -0,0 +1,6 @@
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+
+sudo apt-get update 
+sudo apt-get install mssql-tools unixodbc-dev
