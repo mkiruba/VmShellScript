1d8152641851e72d45d9cacbdb5527d0f05e7f2b
mkiruba <mkiruba@gmail.com>
2022-03-27 22:32:34 +0100

    added bin bash

--- vmshell.sh
+++ vmshell.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mssql-tools unixodbc-dev
